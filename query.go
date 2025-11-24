07ccce651208d679c17e5cd9cc702a2029ee9f08
Eli Ribble <eli@gleipnir.technology>
2025-11-24 18:09:28 +0000

    Add missing query source file

---
+++ query.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	//"github.com/stephenafamo/bob"
+	//"github.com/stephenafamo/bob/dialect/psql"
+)
+
+type QueryWriter interface {
+	WriteQuery(ctx context.Context, w io.Writer, start int) ([]any, error)
+}
+
+func queryToString(query QueryWriter) string {
+	buf := new(bytes.Buffer)
+	_, err := query.WriteQuery(context.TODO(), buf, 0)
+	if err != nil {
+		return fmt.Sprintf("Failed to write query to buffer: %v", err)
+	}
+	return buf.String()
+}
+
+/*
+func insertQueryToString(query bob.BaseQuery[*dialect.InsertQuery]) string {
+	buf := new(bytes.Buffer)
+	_, err := query.WriteQuery(context.TODO(), buf, 0)
+	if err != nil {
+		return fmt.Sprintf("Failed to write query: %v", err)
+	}
+	return buf.String()
+}
+*/
