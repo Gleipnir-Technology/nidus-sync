b31ca6c83ed96dbb1d15c94bcf57cfdb07a11b5a
Eli Ribble <eli@gleipnir.technology>
2025-12-15 15:59:47 +0000

    Bump to version 0.0.5

--- default.nix
+++ default.nix
@@ -7,7 +7,7 @@ pkgs.buildGoModule rec {
         pname = "nidus-sync";
         src = ./.;
         subPackages = [];
-        version = "0.0.4";
+        version = "0.0.5";
         # Needs to be updated after every modification of go.mod/go.sum
         vendorHash = "sha256-7dEwIQMFGhNIMAlu3tiZ3PQoi5fq3sma85d0mEL98E0=";
 }
