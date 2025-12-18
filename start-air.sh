1a055cf296599b85e61d586e6272bd13a12eb32c
Eli Ribble <eli@theribbles.org>
2025-12-18 03:37:42 -0700

    Add simple script to set env vars for running 'air'

---
+++ start-air.sh
@@ -0,0 +1,11 @@
+#!/run/current-system/sw/bin/bash
+ARCGIS_CLIENT_ID=" " \
+ARCGIS_CLIENT_SECRET=" " \
+BASE_URL=" " \
+BIND=" " \
+ENVIRONMENT="DEVELOPMENT" \
+MAPBOX_TOKEN=" " \
+POSTGRES_DSN="postgresql://?host=/var/run/postgresql&dbname=nidus-sync" \
+FIELDSEEKER_SCHEMA_DIRECTORY=" " \
+USER_FILES_DIRECTORY=" " \
+air
