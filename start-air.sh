447e52c6a0eb0e2eb37c6834072b0c1403f398b5
Eli Ribble <eli@theribbles.org>
2026-01-06 14:46:54 +0000

    Read secrets when running air

--- start-air.sh
+++ start-air.sh
@@ -2,10 +2,10 @@
 ARCGIS_CLIENT_ID=" " \
 ARCGIS_CLIENT_SECRET=" " \
 BASE_URL=" " \
-BIND=" " \
+BIND="127.0.0.1:9000" \
 ENVIRONMENT="DEVELOPMENT" \
 MAPBOX_TOKEN=" " \
 POSTGRES_DSN="postgresql://?host=/var/run/postgresql&dbname=nidus-sync" \
 FIELDSEEKER_SCHEMA_DIRECTORY=" " \
 USER_FILES_DIRECTORY=" " \
-air
+export $(cat /var/run/secrets/nidus-dev-sync-env | xargs) && air
