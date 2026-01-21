c2c4d5202609dbaefa4acf172a39b62c3c4cc801
Eli Ribble <eli@gleipnir.technology>
2026-01-21 17:55:04 +0000

    Add tool to clean out test database on public reports

---
+++ tools/delete-all-public-reports.sql
@@ -0,0 +1,7 @@
+DELETE FROM publicreport.image_exif;
+DELETE FROM publicreport.pool_image;
+DELETE FROM publicreport.quick_image;
+DELETE FROM publicreport.pool;
+DELETE FROM publicreport.quick;
+DELETE FROM publicreport.image;
+DELETE FROM publicreport.nuisance;
