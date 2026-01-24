1dc340872cb41d335cf065b988c977040ce2839a
Eli Ribble <eli@gleipnir.technology>
2026-01-24 00:07:04 +0000

    Add initial mock root for public report

--- public-report/routes.go
+++ public-report/routes.go
@@ -12,6 +12,7 @@ func Router() chi.Router {
 	r.Get("/robots.txt", getRobots)
 	r.Get("/email", getEmailByCode)
 	r.Get("/image/{uuid}", getImageByUUID)
+	r.Get("/mock", getMockRoot)
 	r.Get("/nuisance", getNuisance)
 	r.Post("/nuisance-submit", postNuisance)
 	r.Get("/nuisance-submit-complete", getNuisanceSubmitComplete)
