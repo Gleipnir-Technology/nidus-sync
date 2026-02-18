b0ee38898602d52ceb71e1e50a1149bf98cbff08
Eli Ribble <eli@gleipnir.technology>
2026-02-18 08:02:32 +0000

    Add simple example handler for admin functions

--- go.mod
+++ go.mod
@@ -47,6 +47,7 @@ require (
 	github.com/go-ini/ini v1.67.0 // indirect
 	github.com/golang-jwt/jwt/v5 v5.2.2 // indirect
 	github.com/golang/mock v1.6.0 // indirect
+	github.com/gorilla/schema v1.4.1 // indirect
 	github.com/invopop/jsonschema v0.13.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
--- sync/sudo.go
+++ sync/sudo.go
@@ -6,6 +6,8 @@ import (
 
 	"github.com/Gleipnir-Technology/nidus-sync/db/enums"
 	"github.com/Gleipnir-Technology/nidus-sync/db/models"
+	"github.com/gorilla/schema"
+	"github.com/rs/zerolog/log"
 )
 
 type contentSudo struct{}
@@ -19,4 +21,29 @@ func getSudo(ctx context.Context, user *models.User) (string, interface{}, *erro
 	}
 	content := contentAdminDash{}
 	return "sync/sudo.html", content, nil
+}
+
+var decoder = schema.NewDecoder()
+
+type FormSMS struct {
+	Message string `schema:"smsMessage"`
+	Phone   string `schema:"smsPhone"`
+}
+
+func postSudoSMS(w http.ResponseWriter, r *http.Request, u *models.User) {
+	err := r.ParseForm()
+	if err != nil {
+		respondError(w, "Failed to parse form", err, http.StatusInternalServerError)
+		return
+	}
+
+	var sms FormSMS
+
+	err = decoder.Decode(&sms, r.PostForm)
+	if err != nil {
+		respondError(w, "Failed to decode form", err, http.StatusInternalServerError)
+		return
+	}
+	log.Info().Str("msg", sms.Message).Str("phone", sms.Phone).Msg("Got SMS")
+	http.Redirect(w, r, "/sudo", http.StatusFound)
 }
