5e3a97272aa2405f2084360f72ee310ea7135abb
Eli Ribble <eli@gleipnir.technology>
2026-02-25 17:21:37 +0000

    Nest setting object inside URL, use URL in sidebar

--- sync/url.go
+++ sync/url.go
@@ -5,35 +5,46 @@ import (
 )
 
 type contentURL struct {
-	OAuthRefreshArcGIS  string
-	Root                string
-	Route               string
-	SamplePoolCSV       string
-	Setting             string
-	SettingIntegration  string
-	SettingOrganization string
-	SettingPesticide    string
-	SettingPesticideAdd string
-	SettingUser         string
-	SettingUserAdd      string
-	Tegola              string
-	UploadCSVPool       string
+	OAuthRefreshArcGIS string
+	Root               string
+	Route              string
+	SamplePoolCSV      string
+	Setting            contentURLSetting
+	Tegola             string
+	UploadCSVPool      string
+}
+
+type contentURLSetting struct {
+	Fieldseeker  string
+	Integration  string
+	Organization string
+	Pesticide    string
+	PesticideAdd string
+	Root         string
+	User         string
+	UserAdd      string
 }
 
 func newContentURL() contentURL {
 	return contentURL{
-		OAuthRefreshArcGIS:  config.MakeURLNidus("/arcgis/oauth/begin"),
-		Root:                config.MakeURLNidus("/"),
-		Route:               config.MakeURLNidus("/route"),
-		SamplePoolCSV:       config.MakeURLNidus("/static/file/sample-pool.csv"),
-		Setting:             config.MakeURLNidus("/setting"),
-		SettingIntegration:  config.MakeURLNidus("/setting/integration"),
-		SettingOrganization: config.MakeURLNidus("/setting/organization"),
-		SettingPesticide:    config.MakeURLNidus("/setting/pesticide"),
-		SettingPesticideAdd: config.MakeURLNidus("/setting/pesticide/add"),
-		SettingUser:         config.MakeURLNidus("/setting/user"),
-		SettingUserAdd:      config.MakeURLNidus("/setting/user/add"),
-		Tegola:              config.MakeURLTegola("/"),
-		UploadCSVPool:       config.MakeURLNidus("/upload/pool"),
+		OAuthRefreshArcGIS: config.MakeURLNidus("/arcgis/oauth/begin"),
+		Root:               config.MakeURLNidus("/"),
+		Route:              config.MakeURLNidus("/route"),
+		SamplePoolCSV:      config.MakeURLNidus("/static/file/sample-pool.csv"),
+		Setting:            newContentURLSetting(),
+		Tegola:             config.MakeURLTegola("/"),
+		UploadCSVPool:      config.MakeURLNidus("/upload/pool"),
+	}
+}
+func newContentURLSetting() contentURLSetting {
+	return contentURLSetting{
+		Fieldseeker:  config.MakeURLNidus("/setting/integration/fieldseeker"),
+		Integration:  config.MakeURLNidus("/setting/integration"),
+		Organization: config.MakeURLNidus("/setting/organization"),
+		Pesticide:    config.MakeURLNidus("/setting/pesticide"),
+		PesticideAdd: config.MakeURLNidus("/setting/pesticide/add"),
+		Root:         config.MakeURLNidus("/setting"),
+		User:         config.MakeURLNidus("/setting/user"),
+		UserAdd:      config.MakeURLNidus("/setting/user/add"),
 	}
 }
