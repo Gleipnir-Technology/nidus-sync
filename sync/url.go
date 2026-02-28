a5299f0caeaa61fb61754784f74e16355d247378
Eli Ribble <eli@gleipnir.technology>
2026-02-28 23:23:16 +0000

    Re-nest settings sub-pages into configuration

--- sync/url.go
+++ sync/url.go
@@ -22,20 +22,37 @@ func newContentURL() contentURL {
 		Root:               config.MakeURLNidus("/"),
 		Route:              config.MakeURLNidus("/route"),
 		SamplePoolCSV:      config.MakeURLNidus("/static/file/sample-pool.csv"),
-		Setting:            newContentURLSetting(),
 		Sidebar:            newContentURLSidebar(),
 		Tegola:             config.MakeURLTegola("/"),
-		UploadCSVPool:      config.MakeURLNidus("/upload/pool"),
+		UploadCSVPool:      config.MakeURLNidus("/configuration/upload/pool"),
 	}
 }
 
 type contentURLConfiguration struct {
-	Upload string
+	ArcGIS       string
+	Fieldseeker  string
+	Integration  string
+	Organization string
+	Pesticide    string
+	PesticideAdd string
+	Root         string
+	User         string
+	Upload       string
+	UserAdd      string
 }
 
 func newContentURLConfiguration() contentURLConfiguration {
 	return contentURLConfiguration{
-		Upload: config.MakeURLNidus("/configuration/upload"),
+		ArcGIS:       config.MakeURLNidus("/configuration/integration/arcgis"),
+		Fieldseeker:  config.MakeURLNidus("/configuration/integration/fieldseeker"),
+		Integration:  config.MakeURLNidus("/configuration/integration"),
+		Organization: config.MakeURLNidus("/configuration/organization"),
+		Pesticide:    config.MakeURLNidus("/configuration/pesticide"),
+		PesticideAdd: config.MakeURLNidus("/configuration/pesticide/add"),
+		Root:         config.MakeURLNidus("/configuration"),
+		User:         config.MakeURLNidus("/configuration/user"),
+		Upload:       config.MakeURLNidus("/configuration/upload"),
+		UserAdd:      config.MakeURLNidus("/configuration/user/add"),
 	}
 }
 
@@ -56,31 +73,5 @@ func newContentURLSidebar() contentURLSidebar {
 		Operations:    config.MakeURLNidus("/operations"),
 		Planning:      config.MakeURLNidus("/planning"),
 		Review:        config.MakeURLNidus("/review"),
-	}
-}
-
-type contentURLSetting struct {
-	ArcGIS       string
-	Fieldseeker  string
-	Integration  string
-	Organization string
-	Pesticide    string
-	PesticideAdd string
-	Root         string
-	User         string
-	UserAdd      string
-}
-
-func newContentURLSetting() contentURLSetting {
-	return contentURLSetting{
-		ArcGIS:       config.MakeURLNidus("/setting/integration/arcgis"),
-		Fieldseeker:  config.MakeURLNidus("/setting/integration/fieldseeker"),
-		Integration:  config.MakeURLNidus("/setting/integration"),
-		Organization: config.MakeURLNidus("/setting/organization"),
-		Pesticide:    config.MakeURLNidus("/setting/pesticide"),
-		PesticideAdd: config.MakeURLNidus("/setting/pesticide/add"),
-		Root:         config.MakeURLNidus("/setting"),
-		User:         config.MakeURLNidus("/setting/user"),
-		UserAdd:      config.MakeURLNidus("/setting/user/add"),
 	}
 }
