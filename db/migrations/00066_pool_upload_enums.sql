33029908370f4c0fb123e9c0219f3ca964761c0b
Eli Ribble <eli@gleipnir.technology>
2026-03-04 12:12:40 +0000

    Insert enum value rather than renaming
    I must have done something different on my dev machine because the state
    doesn't match staging.

--- db/migrations/00066_pool_upload_enums.sql
+++ db/migrations/00066_pool_upload_enums.sql
@@ -1,5 +1,6 @@
 -- +goose Up
-ALTER TYPE fileupload.PoolConditionType RENAME VALUE 'empty' TO 'dry';
+--ALTER TYPE fileupload.PoolConditionType RENAME VALUE 'empty' TO 'dry';
+ALTER TYPE fileupload.PoolConditionType ADD VALUE 'dry' AFTER 'blue';
 ALTER TYPE fileupload.PoolConditionType ADD VALUE 'false pool' AFTER 'dry';
 ALTER TYPE fileupload.FileStatusType ADD VALUE 'committed' AFTER 'uploaded';
 ALTER TYPE fileupload.FileStatusType ADD VALUE 'discarded' AFTER 'committed';
