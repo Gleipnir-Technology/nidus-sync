e44bc623257b543336637695dea4b0fb4575f07e
Eli Ribble <eli@gleipnir.technology>
2026-03-06 19:01:00 +0000

    Update delete all tool to deal with new foreign keys

--- tools/delete-all-pool-uploads.sql
+++ tools/delete-all-pool-uploads.sql
@@ -3,6 +3,8 @@ BEGIN TRANSACTION;
 	DELETE FROM fileupload.error_csv;
 	DELETE FROM fileupload.csv;
 	DELETE FROM fileupload.error_file;
+	DELETE FROM lead WHERE site_id IN (SELECT id FROM SITE WHERE file_id IS NOT NULL);
+	DELETE FROM site WHERE file_id IS NOT NULL;
 	DELETE FROM fileupload.file;
 COMMIT;
 
