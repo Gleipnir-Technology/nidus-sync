66beb30d938c1d118397f683184e501fb8f73080
Eli Ribble <eli@gleipnir.technology>
2026-03-09 18:29:03 +0000

    Properly encode lat/lng on location

--- platform/types/location.go
+++ platform/types/location.go
@@ -1,6 +1,6 @@
 package types
 
 type Location struct {
-	Latitude  float64 `db:"latitude"`
-	Longitude float64 `db:"longitude"`
+	Latitude  float64 `db:"latitude" json:"latitude"`
+	Longitude float64 `db:"longitude" json:"longitude"`
 }
