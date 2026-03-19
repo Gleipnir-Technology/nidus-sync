ba03bf9d4f06b50887ddc9113f7c8276479cb0e5
Eli Ribble <eli@gleipnir.technology>
2026-03-19 20:14:23 +0000

    Fix audio transcode copy-paste error

--- platform/background/background.go
+++ platform/background/background.go
@@ -13,7 +13,7 @@ import (
 )
 
 func NewAudioTranscode(ctx context.Context, txn bob.Executor, audio_id int32) error {
-	return newJob(ctx, txn, enums.JobtypeCSVCommit, audio_id)
+	return newJob(ctx, txn, enums.JobtypeAudioTranscode, audio_id)
 }
 func NewCSVCommit(ctx context.Context, txn bob.Executor, csv_id int32) error {
 	return newJob(ctx, txn, enums.JobtypeCSVCommit, csv_id)
