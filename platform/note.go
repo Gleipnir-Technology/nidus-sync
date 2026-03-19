17fb3dcdb5e1a0cd2aca32c6bfc87de97ee88815
Eli Ribble <eli@gleipnir.technology>
2026-03-19 20:13:53 +0000

    Fix saving notes from Nidus
    Wow, that's a serious break.

--- platform/note.go
+++ platform/note.go
@@ -11,7 +11,7 @@ import (
 )
 
 func NoteAudioCreate(ctx context.Context, user User, setter models.NoteAudioSetter) error {
-	_, err := models.Organizations.Insert(&setter).One(ctx, db.PGInstance.BobDB)
+	_, err := models.NoteAudios.Insert(&setter).One(ctx, db.PGInstance.BobDB)
 	if err != nil {
 		// Just ignore this failure, it means we already have this content
 		if err.Error() != "insertOrganizationNoteAudios0: ERROR: duplicate key value violates unique constraint \"note_audio_pkey\" (SQLSTATE 23505)" {
@@ -28,7 +28,7 @@ func NoteAudioTranscodedToOgg(uuid string) error {
 	return nil
 }
 func NoteImageCreate(ctx context.Context, user User, setter models.NoteImageSetter) error {
-	err := user.Organization.model.InsertNoteImages(ctx, db.PGInstance.BobDB, &setter)
+	_, err := models.NoteImages.Insert(&setter).One(ctx, db.PGInstance.BobDB)
 	if err == nil {
 		return nil
 	}
