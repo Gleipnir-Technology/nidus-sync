e0a586b3118553cf24073097fea6081b9cf1d92a
Eli Ribble <eli@gleipnir.technology>
2026-03-23 16:09:51 -0700

    Clean up display of planning signal entries

--- ts/components/PlanningColumnListEntry.vue
+++ ts/components/PlanningColumnListEntry.vue
@@ -1,12 +1,17 @@
 <template>
-	<div class="small fw-semibold">{{ title(signal) }}</div>
-	<div class="signal-address">
-		{{ shortAddress(signal.address) }}
+	<div class="row">
+		<div class="col-1">
+			<i class="bi" :class="icon(signal)"></i>
+		</div>
+		<div class="col-5">
+			<div class="small fw-semibold">{{ title(signal) }}</div>
+		</div>
+		<div class="col-6">
+			<div class="text-muted small fst-italic">
+				{{ location(signal) }}
+			</div>
+		</div>
 	</div>
-	<div class="text-muted small">{{ signal.description }}</div>
-	<span v-if="signal.badge" class="badge bg-secondary mt-1">
-		{{ signal.badge }}
-	</span>
 </template>
 
 <script setup lang="ts">
@@ -15,6 +20,20 @@ interface Props {
 	signal: Signal;
 };
 const props = defineProps<Props>();
+function icon(signal: Signal): string {
+	if (signal.type == "flyover pool") {
+		return "bi-water";
+	} else { 
+		return "bi-mosquito";
+	}
+}
+function location(signal: Signal): string {
+	if (signal.address != null) {
+		return shortAddress(signal.address);
+	} else {
+		return `${signal.location.latitude}, ${signal.location.longitude}`;
+	}
+}
 function title(signal: Signal): string {
 	if (signal.type == "flyover pool") {
 		return "Green pool";
