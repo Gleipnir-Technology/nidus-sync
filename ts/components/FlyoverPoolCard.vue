f33020e2b860480e9af3e1369e9ab1b120dee6ec
Eli Ribble <eli@gleipnir.technology>
2026-03-24 09:51:05 -0700

    Add flyover card

---
+++ ts/components/FlyoverPoolCard.vue
@@ -0,0 +1,8 @@
+<template>
+<p>A flyover pool</p>
+<MapProxiedArcgisTile/>
+</template>
+
+<script setup lang="ts">
+import MapProxiedArcgisTile from "@/components/MapProxiedArcgisTile.vue";
+</script>
