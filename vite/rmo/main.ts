64a8de7a32d55c0be9d1266ef1f277c7ae993bb1
Eli Ribble <eli@gleipnir.technology>
2026-04-03 16:00:50 +0000

    Add custom icons to RMO system

--- vite/rmo/main.ts
+++ vite/rmo/main.ts
@@ -1,8 +1,9 @@
 import { createApp } from "vue";
 import { createHead } from "@vueuse/head";
+import "@/gen/custom-icons.scss";
+import "@/style/rmo.scss";
 import router from "@/rmo/router";
 import App from "@/rmo/App.vue";
-import "@/style/rmo.scss";
 
 const app = createApp(App);
 const head = createHead();
