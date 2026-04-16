89a2cb30e679c7e3f2ae6254cd67f5b45eab27c4
Eli Ribble <eli@gleipnir.technology>
2026-04-16 20:40:55 +0000

    Save the type from the database on feature

--- platform/types/feature.go
+++ platform/types/feature.go
@@ -4,5 +4,5 @@ type Feature struct {
 	ID       int32    `db:"id" json:"id"`
 	Location Location `db:"location" json:"location"`
 	SiteID   int32    `db:"site_id" json:"-"`
-	Type     string   `db:"-" json:"type"`
+	Type     string   `db:"type" json:"type"`
 }
