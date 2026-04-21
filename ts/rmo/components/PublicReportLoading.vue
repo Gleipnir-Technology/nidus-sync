bad50a877265e15d48bc4945de5370257b9d30ae
Eli Ribble <eli@gleipnir.technology>
2026-04-21 14:45:11 +0000

    Clean up compliance report creators and share UI

---
+++ ts/rmo/components/PublicReportLoading.vue
@@ -0,0 +1,12 @@
+<template>
+	<div class="container">
+		<div class="row min-vh-100 align-items-center justify-content-center">
+			<div class="col-auto text-center">
+				<div class="spinner-border text-primary" role="status">
+					<span class="visually-hidden">Loading...</span>
+				</div>
+				<p class="mt-3 text-muted">Loading report details...</p>
+			</div>
+		</div>
+	</div>
+</template>
--- ts/rmo/view/ComplianceDistrict.vue
+++ ts/rmo/view/ComplianceDistrict.vue
@@ -1,35 +1,16 @@
 <template>
-	<div class="container">
-		<div class="row min-vh-100 align-items-center justify-content-center">
-			<div class="col-auto text-center">
-				<div class="spinner-border text-primary" role="status">
-					<span class="visually-hidden">Loading...</span>
-				</div>
-				<p class="mt-3 text-muted">Loading report details...</p>
-			</div>
-		</div>
-	</div>
+	<PublicReportLoading />
 </template>
 
 <script setup lang="ts">
-import { computed, onMounted, ref } from "vue";
+import { onMounted } from "vue";
 import { useRouter } from "vue-router";
-import { computedAsync } from "@vueuse/core";
 
-import type { Image } from "@/components/ImageUpload.vue";
 import { useStoreDistrict } from "@/rmo/store/district";
 import { useStoreLocal } from "@/store/local";
 import { useStorePublicReport } from "@/store/publicreport";
-import Intro from "@/rmo/content/compliance/Intro.vue";
-import LoadingOverlay from "@/components/LoadingOverlay.vue";
-import {
-	type ComplianceUpdate,
-	type District,
-	PublicReport,
-	PublicReportCompliance,
-	PublicReportComplianceOptions,
-} from "@/type/api";
-import { Contact, Address, PermissionType } from "@/type/api";
+import PublicReportLoading from "@/rmo/components/PublicReportLoading.vue";
+import { type District } from "@/type/api";
 
 interface Props {
 	slug: string;
