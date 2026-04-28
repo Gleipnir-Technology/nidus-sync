32a0d895c4a473724ffe57cadfae5e89ca47b25c
Eli Ribble <eli@gleipnir.technology>
2026-04-28 06:54:16 +0000

    Fix missing RMO report store

---
+++ ts/rmo/store/publicreport.ts
@@ -0,0 +1,80 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+import { apiClient } from "@/client";
+import {
+	PublicReport,
+	type PublicReportComplianceCreateRequest,
+	type PublicReportDTO,
+	type PublicReportUpdate,
+} from "@/type/api";
+
+export const useStorePublicReport = defineStore("publicreport", () => {
+	// State
+	const _byID = ref<Map<string, PublicReport>>(new Map());
+	const loading = ref(false);
+	//const ongoingFetch = ref<Promise<PublicReport[]> | null>(null);
+
+	function add(pr: PublicReport) {
+		_byID.value.set(pr.public_id, pr);
+	}
+	// Actions
+	async function byID(id: string): Promise<PublicReport> {
+		const r = _byID.value.get(id);
+		if (r) {
+			return r;
+		}
+		return fetchByID(id);
+	}
+	async function byURI(uri: string): Promise<PublicReport> {
+		const id = uri.split("/").pop() || "";
+		if (!id) {
+			throw new Error(`${uri} is not a recognized public report URI`);
+		}
+		return byID(id);
+	}
+	async function createCompliance(
+		data: PublicReportComplianceCreateRequest,
+	): Promise<PublicReport> {
+		const resp = (await apiClient.JSONPost(
+			"/api/rmo/compliance",
+			data,
+		)) as PublicReportDTO;
+		const result = PublicReport.fromJSON(resp);
+		_byID.value.set(result.public_id, result);
+		return result;
+	}
+	async function fetchByID(id: string): Promise<PublicReport> {
+		const uri = `/api/rmo/publicreport/${id}`;
+		return fetchByURI(uri);
+	}
+	async function fetchByURI(uri: string): Promise<PublicReport> {
+		loading.value = true;
+		try {
+			const body = (await apiClient.JSONGet(uri)) as PublicReportDTO;
+			const report = PublicReport.fromJSON(body);
+			_byID.value.set(report.public_id, report);
+			return report;
+		} catch (err) {
+			console.error("Error loading users:", err);
+			throw err;
+		}
+	}
+	async function update(
+		uri: string,
+		updates: PublicReportUpdate,
+	): Promise<PublicReport> {
+		const resp = (await apiClient.JSONPut(uri, updates)) as PublicReportDTO;
+		return PublicReport.fromJSON(resp);
+	}
+	return {
+		// Actions
+		add,
+		byID,
+		byURI,
+		createCompliance,
+		fetchByID,
+		fetchByURI,
+		update,
+	};
+});
