ab5dd13fcbafe7236405a407601355240c8700ef
Eli Ribble <eli@gleipnir.technology>
2026-04-29 15:21:54 +0000

    Make "Submit Anothe Report" go to root so they can choose the type

--- ts/rmo/view/ReportSubmitted.vue
+++ ts/rmo/view/ReportSubmitted.vue
@@ -250,7 +250,7 @@
 
 				<!-- Navigation Links -->
 				<div class="text-center">
-					<RouterLink to="/nuisance" class="btn btn-outline-success">
+					<RouterLink to="/" class="btn btn-outline-success">
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
 							width="16"
